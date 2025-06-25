bd5bda141521617809e5c68ec238854677e0b089
Achrafsadeq <achrafsadeqq@gmail.com>
2025-06-25 16:41:17 -0700

    update

--- utils/redis.js
+++ utils/redis.js
@@ -1,39 +1,43 @@
-import { createClient } from 'redis';
-
+import redis from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
-    this.client = createClient();
-    this.client.on('error', (err) => console.error(`Redis Client Error: ${err}`));
-    this.client.connect();
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.error('Redis Client Error:', err));
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setexAsync = promisify(this.client.setex).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
   }
 
   isAlive() {
-    return this.client && this.client.isOpen;
+    return this.client.connected;
   }
 
   async get(key) {
     try {
-      return await this.client.get(key);
+      return await this.getAsync(key);
     } catch (err) {
-      console.error(`Redis GET error: ${err}`);
-      return null;
+      console.error('Redis get error:', err);
+      throw err;
     }
   }
 
   async set(key, value, duration) {
     try {
-      await this.client.setEx(key, duration, value);
+      await this.setexAsync(key, duration, value);
     } catch (err) {
-      console.error(`Redis SET error: ${err}`);
+      console.error('Redis set error:', err);
+      throw err;
     }
   }
 
   async del(key) {
     try {
-      await this.client.del(key);
+      await this.delAsync(key);
     } catch (err) {
-      console.error(`Redis DEL error: ${err}`);
+      console.error('Redis del error:', err);
+      throw err;
     }
   }
 }
